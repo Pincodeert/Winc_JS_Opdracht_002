0c6aff9098c7e04cdad10cd622fb38e59e51a074
Pincodeert <84202600+Pincodeert@users.noreply.github.com>
2021-07-05 22:14:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log('hallo Winc Academy studenten')
+// dit is een grote som
+/* dit is een blockcomment. gemaakt voor een oefening.
+console.log(1230941*1823794)
+console.log(637263/54)
+*/
